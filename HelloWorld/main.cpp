7646f448875b987982384f6330fbb24e39da1740
Tom-Quirk <thomas.q@hotmail.co.uk>
2023-07-16 15:51:49 +0100

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Tom Quirk on 16/07/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
